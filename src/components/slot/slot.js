2830fc46da9761207b0245f05a13d1b2aa657fbc
Darko <draguz1512@gmail.com>
2021-02-03 20:27:28 +0100

    Add slot intent buttons

--- src/components/slot/slot.js
+++ src/components/slot/slot.js
@@ -3,26 +3,39 @@ Vue.component('app-slot', {
   <div>
     <h4>{{appSlot.name}}</h4>
     If a user fails to provide {{appSlot.name}} value, Kitchen plus will ask him to fill that value.
-    <div v-if="elicitationSlot?.prompts?.elicitation">
+    <div class="slot-elicitation-container" v-if="elicitationSlot?.prompts?.elicitation">
       Kitchen plus will prompt for the slot in one of the following ways:
       <div v-for="(prompt, promptIndex) in promptsElicitation?.variations">
-        <input type="text" v-model="promptsElicitation.variations[promptIndex].value"></input>
+        <input class="input-text" type="text" v-model="promptsElicitation.variations[promptIndex].value"></input>
+        <button v-on:click="removeSlotElicitation(promptIndex)">-</button>
       </div>
+      <button v-on:click="addSlotElicitation()">+</button>
     </div>
     After Kitchen Plus prompts, user can fill the {{appSlot.name}} in one of the following ways:
     <div v-if="appSlot?.samples?.length">
       <div v-for="(sample, index) in appSlot.samples">
         <input type="text" v-model="appSlot.samples[index]"></input>
+        <button v-on:click="removeSlotSample(promptIndex)">-</button>
       </div>
+      <button v-on:click="addSlotSample()">+</button>
     </div>
   </div>`,
   props: ['app-slot', 'elicitation-slot', 'prompts-elicitation'],
   methods: {
-    addSample: function() {
-      this.samples.push('');
+    addSlotElicitation: function() {
+      this.promptsElicitation.variations.push({
+        type: 'PlainText',
+        value: ''
+      });
     },
-    removeSample: function(index) {
-      this.samples.splice(index, 1);
+    removeSlotElicitation: function(index) {
+      this.promptsElicitation.variations.splice(index, 1);
+    },
+    addSlotSample: function() {
+      this.appSlot.push('');
+    },
+    removeSlotSample: function(index) {
+      this.appSlot.samples.splice(index, 1);
     }
   }
 });
